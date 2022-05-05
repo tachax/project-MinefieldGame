f2a7309932b0cb292a10d3f8101a795153052c1b
Natacha Angheben <natiangheben@outlook.com>
2022-05-05 17:11:44 -0300

    tabuleiro

--- facil/facil.js
+++ facil/facil.js
@@ -1,21 +1,20 @@
-var tabuleiro = []
-var linhas
-var colunas
-linhas = 8
-colunas = 10
+var tabuleiro = [];
+var linhas;
+var colunas;
+linhas = 8;
+colunas = 10;
 
-function oi() {
-  //inserindo a qnt de linhas e colunas
-  for (let i = 0; i < linhas; i++) {
-    //linha
-    tabuleiro[tabuleiro.length] = []
-    document.getElementById('tabela').innerHTML += `<tr id="${i}"></tr>`
-    for (let j = 0; j < colunas; j++) {
-      //coluna c/ os números
-      tabuleiro[i][j] = 0
-      document.getElementById(
-        i
-      ).innerHTML += `<td id="${i}${j}">${tabuleiro[i][j]}</td>`
-    }
+//inserindo a tabela
+document.write(`<table class='table' border='1' id='tabela'></table>`);
+
+//inserindo a qnt de linhas e colunas
+for (let i = 0; i < linhas; i++) {
+  //linha
+  tabuleiro[tabuleiro.length] = [];
+  document.getElementById('tabela').innerHTML += `<tr id="${i}"></tr>`;
+  for (let j = 0; j < colunas; j++) {
+    //coluna c/ os números
+    tabuleiro[i][j] = 0;
+    document.getElementById(i).innerHTML += `<td id="${i}${j}">${tabuleiro[i][j]}</td>`;
   }
 }
